3153d05f3fe07d1b5fedca5307f44691a35b7758
DanGould <d@ngould.dev>
2024-11-13 13:57:42 -0500

    Bind to uniffi using proc macros
    Proc macros let rust-analyzer check that types are compatible
    at rust compile time, and thus let downstream projects more easily
    depend on bitcoin-ffi.
    
    The `Network` enum is defined in the UDL file, but we need to define it
    in rust with `uniffi::Enum` in order to check UniFFI bindings at rust
    compile time.
    
    "It is permitted to use this [`uniffi::Enum`] macro on a type that is
    also defined in the UDL file as long as the two definitions are equal in
    the names and ordering of variants and variant fields"
    
    see: https://mozilla.github.io/uniffi-rs/latest/proc_macro/index.html#the-uniffienum-derive

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,6 @@ use bitcoin::TxIn as BitcoinTxIn;
 use bitcoin::TxOut as BitcoinTxOut;
 
 pub use bitcoin::BlockHash;
-pub use bitcoin::OutPoint;
 pub use bitcoin::Txid;
 
 use error::AddressParseError;
@@ -26,20 +25,25 @@ use std::sync::Arc;
 #[macro_use]
 mod macros;
 pub mod error;
-pub use bitcoin::Network;
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, uniffi::Object)]
 pub struct Address(BitcoinAddress<NetworkChecked>);
 
+#[uniffi::export]
 impl Address {
+    #[uniffi::constructor]
     pub fn new(address: String, network: Network) -> Result<Self, AddressParseError> {
         let parsed_address = BitcoinAddress::from_str(&address).map_err(AddressParseError::from)?;
-        let network_checked_address = parsed_address.require_network(network)?;
+        let network_checked_address = parsed_address.require_network(network.into())?;
         Ok(Address(network_checked_address))
     }
 
+    #[uniffi::constructor]
     pub fn from_script(script: Arc<Script>, network: Network) -> Result<Self, FromScriptError> {
-        let address = BitcoinAddress::from_script(&script.0.clone(), network)?;
+        let address = BitcoinAddress::from_script(
+            &script.0.clone(),
+            Into::<bitcoin::Network>::into(network),
+        )?;
         Ok(Address(address))
     }
 
@@ -54,7 +58,7 @@ impl Address {
     pub fn is_valid_for_network(&self, network: Network) -> bool {
         let address_str = self.0.to_string();
         if let Ok(unchecked_address) = address_str.parse::<BitcoinAddress<NetworkUnchecked>>() {
-            unchecked_address.is_valid_for_network(network)
+            unchecked_address.is_valid_for_network(network.into())
         } else {
             false
         }
@@ -71,10 +75,12 @@ type CheckedBitcoinAddress = BitcoinAddress<NetworkChecked>;
 impl_from_core_type!(Address, CheckedBitcoinAddress);
 impl_from_ffi_type!(Address, CheckedBitcoinAddress);
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, uniffi::Object)]
 pub struct FeeRate(pub BitcoinFeeRate);
 
+#[uniffi::export]
 impl FeeRate {
+    #[uniffi::constructor]
     pub fn from_sat_per_vb(sat_per_vb: u64) -> Result<Self, FeeRateError> {
         let fee_rate: Option<BitcoinFeeRate> = BitcoinFeeRate::from_sat_per_vb(sat_per_vb);
         match fee_rate {
@@ -83,6 +89,7 @@ impl FeeRate {
         }
     }
 
+    #[uniffi::constructor]
     pub fn from_sat_per_kwu(sat_per_kwu: u64) -> Self {
         FeeRate(BitcoinFeeRate::from_sat_per_kwu(sat_per_kwu))
     }
@@ -103,10 +110,32 @@ impl FeeRate {
 impl_from_core_type!(FeeRate, BitcoinFeeRate);
 impl_from_ffi_type!(FeeRate, BitcoinFeeRate);
 
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Clone, Debug, PartialEq, Eq, uniffi::Record)]
+pub struct OutPoint {
+    pub txid: Txid,
+    pub vout: u32,
+}
+impl From<bitcoin::OutPoint> for OutPoint {
+    fn from(outpoint: bitcoin::OutPoint) -> Self {
+        OutPoint {
+            txid: outpoint.txid,
+            vout: outpoint.vout,
+        }
+    }
+}
+
+impl From<OutPoint> for bitcoin::OutPoint {
+    fn from(outpoint: OutPoint) -> Self {
+        bitcoin::OutPoint::new(outpoint.txid, outpoint.vout)
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, uniffi::Object)]
 pub struct Script(pub BitcoinScriptBuf);
 
+#[uniffi::export]
 impl Script {
+    #[uniffi::constructor]
     pub fn new(raw_output_script: Vec<u8>) -> Self {
         let script: BitcoinScriptBuf = raw_output_script.into();
         Script(script)
@@ -120,7 +149,7 @@ impl Script {
 impl_from_core_type!(Script, BitcoinScriptBuf);
 impl_from_ffi_type!(Script, BitcoinScriptBuf);
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, uniffi::Record)]
 pub struct TxOut {
     pub value: Arc<Amount>,
     pub script_pubkey: Arc<Script>,
@@ -144,14 +173,17 @@ impl From<TxOut> for BitcoinTxOut {
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, uniffi::Object)]
 pub struct Amount(pub BitcoinAmount);
 
+#[uniffi::export]
 impl Amount {
+    #[uniffi::constructor]
     pub fn from_sat(sat: u64) -> Self {
         Amount(BitcoinAmount::from_sat(sat))
     }
 
+    #[uniffi::constructor]
     pub fn from_btc(btc: f64) -> Result<Self, ParseAmountError> {
         let bitcoin_amount = BitcoinAmount::from_btc(btc).map_err(ParseAmountError::from)?;
         Ok(Amount(bitcoin_amount))
@@ -169,7 +201,7 @@ impl Amount {
 impl_from_core_type!(Amount, BitcoinAmount);
 impl_from_ffi_type!(Amount, BitcoinAmount);
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, uniffi::Record)]
 pub struct TxIn {
     pub previous_output: OutPoint,
     pub script_sig: Arc<Script>,
@@ -180,7 +212,7 @@ pub struct TxIn {
 impl From<BitcoinTxIn> for TxIn {
     fn from(tx_in: BitcoinTxIn) -> Self {
         TxIn {
-            previous_output: tx_in.previous_output,
+            previous_output: tx_in.previous_output.into(),
             script_sig: Arc::new(tx_in.script_sig.into()),
             sequence: tx_in.sequence.0,
             witness: tx_in.witness.to_vec(),
@@ -191,7 +223,7 @@ impl From<BitcoinTxIn> for TxIn {
 impl From<&BitcoinTxIn> for TxIn {
     fn from(tx_in: &BitcoinTxIn) -> Self {
         TxIn {
-            previous_output: tx_in.previous_output,
+            previous_output: tx_in.previous_output.into(),
             script_sig: Arc::new(tx_in.script_sig.clone().into()),
             sequence: tx_in.sequence.0,
             witness: tx_in.witness.to_vec(),
@@ -202,7 +234,7 @@ impl From<&BitcoinTxIn> for TxIn {
 impl From<TxIn> for BitcoinTxIn {
     fn from(tx_in: TxIn) -> Self {
         BitcoinTxIn {
-            previous_output: tx_in.previous_output,
+            previous_output: tx_in.previous_output.into(),
             script_sig: tx_in.script_sig.0.clone(),
             sequence: Sequence(tx_in.sequence),
             witness: tx_in.witness.into(),
@@ -210,10 +242,12 @@ impl From<TxIn> for BitcoinTxIn {
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, uniffi::Object)]
 pub struct Transaction(pub BitcoinTransaction);
 
+#[uniffi::export]
 impl Transaction {
+    #[uniffi::constructor]
     pub fn deserialize(transaction_bytes: &[u8]) -> Result<Self, EncodeError> {
         let transaction: BitcoinTransaction = deserialize(transaction_bytes)?;
         Ok(Transaction(transaction))
@@ -281,7 +315,42 @@ impl Transaction {
 impl_from_core_type!(Transaction, BitcoinTransaction);
 impl_from_ffi_type!(Transaction, BitcoinTransaction);
 
-impl_string_custom_typedef!(BlockHash);
-impl_string_custom_typedef!(Txid);
+#[derive(Clone, Default, uniffi::Enum)]
+#[non_exhaustive]
+pub enum Network {
+    #[default]
+    Bitcoin,
+    Testnet,
+    Signet,
+    Regtest,
+}
 
-uniffi::include_scaffolding!("bitcoin");
+impl From<Network> for bitcoin::Network {
+    fn from(network: Network) -> Self {
+        match network {
+            Network::Bitcoin => bitcoin::Network::Bitcoin,
+            Network::Testnet => bitcoin::Network::Testnet,
+            Network::Signet => bitcoin::Network::Signet,
+            Network::Regtest => bitcoin::Network::Regtest,
+        }
+    }
+}
+
+impl From<bitcoin::Network> for Network {
+    fn from(network: bitcoin::Network) -> Self {
+        match network {
+            bitcoin::Network::Bitcoin => Network::Bitcoin,
+            bitcoin::Network::Testnet => Network::Testnet,
+            bitcoin::Network::Signet => Network::Signet,
+            bitcoin::Network::Regtest => Network::Regtest,
+            _ => unreachable!(),
+        }
+    }
+}
+
+uniffi::custom_type!(Txid, String);
+impl_string_custom_typedef!(Txid);
+uniffi::custom_type!(BlockHash, String);
+impl_string_custom_typedef!(BlockHash);
+
+uniffi::setup_scaffolding!("bitcoin");
